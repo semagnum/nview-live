e0fd348a3febda135902618712314d06fe44768d
Spencer Magnusson <semagnum@gmail.com>
2023-02-05 09:58:34 -0500

    Updated licensing

--- frame_handler.py
+++ frame_handler.py
@@ -1,3 +1,18 @@
+# Copyright (C) 2023 Spencer Magnusson
+# semagnum@gmail.com
+# Created by Spencer Magnusson
+#     This program is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#     This program is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+#     You should have received a copy of the GNU General Public License
+#     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 from typing import Iterable
 
 import bpy
@@ -9,10 +24,10 @@ from .budget import budget_factory
 
 def vert_count(obj: bpy.types.Object, collection_vert_count_cache: dict) -> int:
     """Counts vertices for a given object, including instanced collections.
+    Optimizes lookups when the same collection is referenced multiple times.
 
     :param obj: object to check vertex count
     :param collection_vert_count_cache: cache of collections and their vertex counts.
-    Optimizes lookups when the same collection is referenced multiple times.
     """
     if obj.type == 'MESH':
         return len(obj.data.vertices)
